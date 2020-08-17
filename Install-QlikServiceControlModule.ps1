dba3fa6a882ef1a13182cccae8dec4a17d0d67c0
tonikautto <toni@kautto.se>
2020-08-17 13:19:57 +1000

    Helper file added

--- Install-QlikServiceControlModule.ps1
+++ Install-QlikServiceControlModule.ps1
@@ -32,9 +32,32 @@ try{
             git clone $repo_path
         }    
         
+    # } else {
+
+    #     $DownloadsPath = "$env:HOMEDRIVE$env:HOMEPATH\Downloads"
+    
+    #     Write-Host "Downloading files from GitHub to $downloads_folder..." -ForegroundColor Green      
+
+    #     try {            
+    #         Invoke-WebRequest -uri "https://github.com/qlikcustomersuccess/Qlik-Service-Control/archive/master.zip" -OutFile "$DownloadsPath\Qlik-Service-Control.zip"
+    #         Write-Host "$ModulePath"
+    #         if(Test-Path -Path "$ModulePath\Qlik-Service-Control") {
+    #             Remove-Item "$ModulePath\Qlik-Service-Control" -Recurse -Force
+    #         }                
+    #         Move-Item -Path "$DownloadsPath\Qlik-Service-Control.zip" -Destination "$ModulePath"
+    #         Expand-Archive -Path "$ModulePath\Qlik-Service-Control.zip" -DestinationPath "$ModulePath" -Force
+    #         Move-Item -Path "$ModulePath\Qlik-Service-Control-master" -Destination "$ModulePath\Qlik-Service-Control"
+    #     }
+    #     catch {
+    #         Write-Host "Download failed!" -ForegroundColor Red
+    #         Exit
+    #     }
+    
     }
 } catch {
     Set-Location $CurrentPath
     Throw "ERROR: Install/Update failed! Git is missing or repository path is inaccessible $repo_path"    
 }
 Set-Location $CurrentPath
+
+Import-Module "$ModulePath\Qlik-Service-Control\Qlik-Service-Control.psd1"
